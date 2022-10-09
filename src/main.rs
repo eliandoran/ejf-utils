0665351d24885acd21ad4bb6ee278c066e6dcae3
Elian Doran <contact@eliandoran.me>
2022-10-09 17:22:56 +0300

    Align characters to baseline & calculate image height properly

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,20 @@ const FACE_HORIZONTAL_RESOLUTION: u32 = 100;
 const SKIP_CONTROL_CHARACTERS: bool = true;
 const PRINT_CHARACTERS: bool = false;
 
-fn get_pixels(bitmap: Bitmap, size: (u32, u32)) -> DynamicImage {    
+fn get_pixels(bitmap: Bitmap, size: (u32, u32), offset_y: i32) -> DynamicImage {    
     let mut figure = ImageBuffer::new(size.0, size.1);
     let width = bitmap.width() as usize;
+    let image_height = size.1;
     
     for cx in 0..width {
         for cy in 0..bitmap.rows() as usize {
             let pos = (cy) * width + (cx);
             let pixel = [ bitmap.buffer()[pos] ];
-            figure[(cx as u32, cy as u32)] = image::Luma(pixel);
+
+            let dest_y = cy as i32 + offset_y;
+            if dest_y >= 0 && dest_y < image_height as i32 {
+                figure[(cx as u32, dest_y as u32)] = image::Luma(pixel);
+            }
         }
     }
 
@@ -26,7 +31,7 @@ fn get_pixels(bitmap: Bitmap, size: (u32, u32)) -> DynamicImage {
     image
 }
 
-fn render_single_character(face: &Face, ch: char, size: (u32, u32)) {
+fn render_single_character(face: &Face, ch: char, size: (u32, u32), max_ascent: u32) {
     // Try to render a single character.
     face.load_char(ch as usize, LoadFlag::RENDER)
         .expect("Unable to load one of the characters for rendering.");
@@ -38,7 +43,8 @@ fn render_single_character(face: &Face, ch: char, size: (u32, u32)) {
     println!("Bitmap position: char='{}', x={}, y={}", ch, x, y);
 
     // Get the pixels of that single character.
-    let img = get_pixels(glyph.bitmap(), size);    
+    let offset_y = max_ascent as i32 - (glyph.bitmap_top() as i32);
+    let img = get_pixels(glyph.bitmap(), size, offset_y);    
 
     // Save the output to png.
     let filename = format!("output/0x{:x}.png", ch as u32);
@@ -66,25 +72,33 @@ fn main() {
     face.set_char_size(FACE_CHAR_WIDTH, 0, FACE_HORIZONTAL_RESOLUTION, 0)
         .expect("Unable to set the character size.");
 
+    // Determine ascent, descent, image height
+    let y_scale = face.size_metrics().unwrap().y_scale as f32;
+    let max_ascent = (face.ascender() as f32 * (y_scale / 65536.0)) as u32 >> 6;
+    
     // Determine max height.
-    let mut max_height = 0;
     let mut max_width: u32 = 0;
+    let mut max_descent: i32 = 0;
     for code in (0 as u8)..(255 as u8) {
         face.load_char(code as usize, LoadFlag::RENDER)
             .expect("Unable to load character.");
-        let bitmap = face.glyph().bitmap();
+        let glyph = face.glyph();
+        let bitmap = glyph.bitmap();
         let cur_width = bitmap.width() as u32;
-        let cur_height = bitmap.rows() as u32;
+        let cur_descent = (glyph.metrics().height >> 6) as i32 - glyph.bitmap_top();
+
         if cur_width > max_width {
             max_width = cur_width;
+        }       
+        if cur_descent > max_descent {
+            max_descent = cur_descent;
         }
-        if cur_height > max_height {
-            max_height = cur_height;
-        }        
     }
 
+    println!("{}", max_descent);
+    let image_height = (max_ascent as i32 + max_descent) as u32;
+
     println!("Font family: {}", face.family_name().unwrap());
-    println!("Image dimensions: width={}px, height={}px", max_width, max_height);
 
     // Render the characters.
     for code in (0 as u8)..(255 as u8) {
@@ -94,7 +108,7 @@ fn main() {
             continue;
         }
 
-        render_single_character(&face, ch as char, (max_width, max_height));
+        render_single_character(&face, ch as char, (max_width, image_height), max_ascent);
     }    
 }
 
