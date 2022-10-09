091cf17fc42f4916f43a1fc7d108183635d1c0bc
Elian Doran <contact@eliandoran.me>
2022-10-09 14:01:40 +0300

    Render multiple character & fix overflow

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,22 @@ use image::{DynamicImage, ImageBuffer, ImageFormat};
 use viuer::Config;
 
 const FONT_PATH: &'static str = "./fonts/Roboto/Roboto-Light.ttf";
-const FACE_CHAR_WIDTH: isize = 10 * 64;
+const FACE_CHAR_WIDTH: isize = 8 * 64;
 const FACE_HORIZONTAL_RESOLUTION: u32 = 100;
-const RENDER_CHAR: char = 'A';
 
 const OUTPUT_WIDTH: u32 = 32;
 const OUTPUT_HEIGHT: u32 = 24;
 
-fn get_pixels(bitmap: Bitmap, x: u32, y: u32) -> DynamicImage {
+fn get_pixels(bitmap: Bitmap, x: i32, y: i32) -> DynamicImage {
     let mut figure = ImageBuffer::new(OUTPUT_WIDTH as u32, OUTPUT_HEIGHT as u32);
     let mut p = 0;
     let mut q = 0;
     let width = bitmap.width() as usize;
-    let x_max: u32 = x + width as u32;
-    let y_max: u32 = y + bitmap.rows() as u32;    
+    let x_max: u32 = (x + width as i32) as u32;
+    let y_max: u32 = (y + bitmap.rows() as i32) as u32;
 
-    for i in x..x_max {
-        for j in y..y_max {
+    for i in (x as u32)..x_max {
+        for j in (y as u32)..y_max {
             if i < OUTPUT_WIDTH && j < OUTPUT_HEIGHT {
                 figure[(i,j)] = image::Luma([bitmap.buffer()[q * width + p]]);
                 q += 1;
@@ -34,27 +33,16 @@ fn get_pixels(bitmap: Bitmap, x: u32, y: u32) -> DynamicImage {
     image
 }
 
-fn main() {
-    // Try to open the font.
-    let library = Library::init().unwrap();
-    let face: Face = library
-        .new_face(FONT_PATH, 0)
-        .expect("Unable to load the font file to be used for rendering.\nPlease check the path to the font, permissions or that the file format is supported.");
-
-    // Set face properties.
-    face.set_char_size(FACE_CHAR_WIDTH, 0, FACE_HORIZONTAL_RESOLUTION, 0)
-        .expect("Unable to set the character size.");
-
+fn render_single_character(face: &Face, ch: char) {
     // Try to render a single character.
-    face.load_char(RENDER_CHAR as usize, LoadFlag::RENDER)
+    face.load_char(ch as usize, LoadFlag::RENDER)
         .expect("Unable to load one of the characters for rendering.");
 
     let glyph = face.glyph();
-    let x = glyph.bitmap_left() as u32;
-    let y = glyph.bitmap_top() as u32;
+    let x = glyph.bitmap_left();
+    let y = glyph.bitmap_top();
     
-    println!("Font family: {}", face.family_name().unwrap());
-    println!("Bitmap position: x={}, y={}", x, y);
+    println!("Bitmap position: char='{}', x={}, y={}", ch, x, y);
 
     // Get the pixels of that single character.
     let img = get_pixels(glyph.bitmap(), x, y);    
@@ -69,5 +57,23 @@ fn main() {
 
     viuer::print(&img, &config)
         .expect("Image printing failed.");
+}
+
+fn main() {
+    // Try to open the font.
+    let library = Library::init().unwrap();
+    let face: Face = library
+        .new_face(FONT_PATH, 0)
+        .expect("Unable to load the font file to be used for rendering.\nPlease check the path to the font, permissions or that the file format is supported.");
+
+    println!("Font family: {}", face.family_name().unwrap());
+
+    // Set face properties.
+    face.set_char_size(FACE_CHAR_WIDTH, 0, FACE_HORIZONTAL_RESOLUTION, 0)
+        .expect("Unable to set the character size.");
+
+    for ch in 'a'..'z' {
+        render_single_character(&face, ch);
+    }    
 }
 
