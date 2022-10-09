2a7a6df001c1d5a7ef5033ef74d75e136354a4ff
Elian Doran <contact@eliandoran.me>
2022-10-09 12:20:48 +0300

    Try to load font file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use freetype::{Library, Face};
+
+const FONT_PATH: &'static str = "./fonts/Roboto/Roboto-Light.ttf";
+
+const ERROR_UNABLE_TO_LOAD_FONT_FILE: &'static str = "Unable to load the font file to be used for rendering.\nPlease check the path to the font, permissions or that the file format is supported.";
+
 fn main() {
-    println!("Hello, world!");
+    // Try to open the font.
+    let library = Library::init().unwrap();
+    let face: Face = library
+        .new_face(FONT_PATH, 0)
+        .expect(ERROR_UNABLE_TO_LOAD_FONT_FILE);
+
+    println!("Font family: {}", face.family_name().unwrap());
 }
+
