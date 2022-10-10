999f17eadffcc8c526e6f331ef23777bc74d6b6e
Elian Doran <contact@eliandoran.me>
2022-10-10 19:41:21 +0300

    Separate the renderer into its own module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use binstall_zip::{ZipWriter, write::FileOptions, CompressionMethod};
-use freetype::{Library, Face, face::LoadFlag, Bitmap};
-use image::{DynamicImage, ImageBuffer, ImageFormat, RgbImage};
+use freetype::{Library, Face, face::LoadFlag};
+use image::ImageFormat;
 use indicatif::ProgressBar;
-use viuer::Config;
-use core::{cmp::max};
+
 use std::{fs::File, io::{Write, Cursor}};
 
 mod header;
+mod renderer;
 
 const FONT_PATH: &'static str = "./fonts/Roboto/Roboto-Light.ttf";
 const FACE_CHAR_WIDTH: isize = 25 * 64;
@@ -14,52 +14,6 @@ const FACE_HORIZONTAL_RESOLUTION: u32 = 100;
 
 const SKIP_CONTROL_CHARACTERS: bool = true;
 const PRINT_CHARACTERS: bool = false;
-
-fn get_pixels(bitmap: Bitmap, image_height: u32, offset_y: i32) -> DynamicImage {    
-    let width = bitmap.width() as usize;
-    let image_width = max(1, width as u32); // 0px width images are not allowed.
-    let mut figure = ImageBuffer::new(image_width, image_height);
-    
-    for cx in 0..width {
-        for cy in 0..bitmap.rows() as usize {
-            let pos = (cy) * width + (cx);
-            let pixel = [ bitmap.buffer()[pos] ];
-
-            let dest_y = cy as i32 + offset_y;
-            if dest_y >= 0 && dest_y < image_height as i32 {
-                figure[(cx as u32, dest_y as u32)] = image::Luma(pixel);
-            }
-        }
-    }
-
-    let mut image = DynamicImage::ImageLuma8(figure);
-    image.invert();
-    image 
-}
-
-fn render_single_character(face: &Face, ch: char, image_height: u32, max_ascent: u32) -> RgbImage {
-    // Try to render a single character.
-    face.load_char(ch as usize, LoadFlag::RENDER)
-        .expect("Unable to load one of the characters for rendering.");
-
-    let glyph = face.glyph();
-
-    // Get the pixels of that single character.
-    let offset_y = max_ascent as i32 - (glyph.bitmap_top() as i32);
-    let img = get_pixels(glyph.bitmap(), image_height, offset_y);    
-
-    if PRINT_CHARACTERS {
-        let config = Config {
-            absolute_offset: false,
-            ..Default::default()
-        };
-
-        viuer::print(&img, &config)
-            .expect("Image printing failed.");
-    }
-
-    img.to_rgb8()
-}
 
 fn main() {
     // Open the output file
@@ -111,16 +65,20 @@ fn main() {
             continue;
         }
 
-        let image = render_single_character(&face, ch as char, image_height, max_ascent);
+        let image = renderer::render_single_character(&face, ch as char, image_height, max_ascent);
         let mut cursor = Cursor::new(Vec::new());
-        
-        image.write_to(&mut cursor, ImageFormat::Png).unwrap();
+
+        if PRINT_CHARACTERS {
+            renderer::print_character(&image);
+        }
+
+        image.to_rgb8().write_to(&mut cursor, ImageFormat::Png).unwrap();
         
         // Write the character to the zip file
         let char_code = format!("0x{:x}", ch as u32);
         let image_data = cursor.into_inner();
         zip.start_file(&char_code, zip_options).unwrap();
-        zip.write(&image_data).unwrap();
+        zip.write(&image_data).unwrap();        
 
         // Also write the "design" character to the zip file.
         zip.start_file(format!("design_{}", &char_code), zip_options).unwrap();
---
+++ src/renderer.rs
@@ -0,0 +1,50 @@
+use viuer::Config;
+use core::{cmp::max};
+use image::{DynamicImage, ImageBuffer};
+use freetype::{Face, Bitmap, face::LoadFlag};
+
+pub fn get_pixels(bitmap: Bitmap, image_height: u32, offset_y: i32) -> DynamicImage {    
+    let width = bitmap.width() as usize;
+    let image_width = max(1, width as u32); // 0px width images are not allowed.
+    let mut figure = ImageBuffer::new(image_width, image_height);
+    
+    for cx in 0..width {
+        for cy in 0..bitmap.rows() as usize {
+            let pos = (cy) * width + (cx);
+            let pixel = [ bitmap.buffer()[pos] ];
+
+            let dest_y = cy as i32 + offset_y;
+            if dest_y >= 0 && dest_y < image_height as i32 {
+                figure[(cx as u32, dest_y as u32)] = image::Luma(pixel);
+            }
+        }
+    }
+
+    let mut image = DynamicImage::ImageLuma8(figure);
+    image.invert();
+    image 
+}
+
+pub fn render_single_character(face: &Face, ch: char, image_height: u32, max_ascent: u32) -> DynamicImage {
+    // Try to render a single character.
+    face.load_char(ch as usize, LoadFlag::RENDER)
+        .expect("Unable to load one of the characters for rendering.");
+
+    let glyph = face.glyph();
+
+    // Get the pixels of that single character.
+    let offset_y = max_ascent as i32 - (glyph.bitmap_top() as i32);
+    let img = get_pixels(glyph.bitmap(), image_height, offset_y);    
+
+    img
+}
+
+pub fn print_character(img: &DynamicImage) {
+    let config = Config {
+        absolute_offset: false,
+        ..Default::default()
+    };
+
+    viuer::print(&img, &config)
+        .expect("Image printing failed.");
+}
