facb548a0ab5997e283c369c31907c95218498b3
Elian Doran <contact@eliandoran.me>
2023-02-25 09:55:32 +0200

    Create basic Nix flake
    The build fails due to missing cmake for
    freetype, but it's a start.

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+let
+  pname = "ejf-utils";
+in {
+  app = pkgs.rustPlatform.buildRustPackage {
+    pname = pname;
+    version = "0.0.1";
+    src = ./.;
+    cargoBuildFlags = "-p ${pname}";
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+    };
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "MicroEJ font file generator using a declarative syntax, written in Rust.";
+  inputs = {
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.follows = "rust-overlay/flake-utils";
+    nixpkgs.follows = "rust-overlay/nixpkgs";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        code = pkgs.callPackage ./. { inherit nixpkgs system rust-overlay; };
+      in rec {
+        packages = {
+          app = code.app;
+          default = code.app;
+        };
+      }
+    );
+}
